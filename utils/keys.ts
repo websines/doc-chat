a75df4ecd91e22a096b09d084a9936e6262f4add
websines <to.subh03@gmail.com>
2023-08-02 23:12:46 +0530

    final

--- utils/keys.ts
+++ utils/keys.ts
@@ -1,8 +1,8 @@
 
-export const openAIapiKey = process.env.OPENAI_API_KEY;
-export  const pineconeApiKey = process.env.PINECONE_API_KEY;
-export const pineconeEnvironment = process.env.PINECONE_ENVIRONMENT
-export  const pineconeIndexName = process.env.PINECONE_INDEX_NAME
+export const openAIapiKey = process.env.OPENAI_API_KEY || "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";
+export  const pineconeApiKey = process.env.PINECONE_API_KEY || "23e48064-f588-4271-9ce2-632ade12ebcd";
+export const pineconeEnvironment = process.env.PINECONE_ENVIRONMENT || "asia-southeast1-gcp-free"
+export  const pineconeIndexName = process.env.PINECONE_INDEX_NAME || "langchainchatbot"
  
 export const supabaseKey = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoaGtmaXdzeWl3cWFxZGNuZ2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAzOTYwMTUsImV4cCI6MjAwNTk3MjAxNX0.hjLAtbHmdc3E9SBu3T_K1JR0WOJ3t8c9wNfrQhCRFAs"
 export const supabaseURL = process.env.SUPABASE_URL || "https://zhhkfiwsyiwqaqdcngcx.supabase.co"
