11d48337705f5c57e9760c001bc1da3978c2ab9f
websines <to.subh03@gmail.com>
2023-08-03 18:43:25 +0530

    no-key

--- utils/keys.ts
+++ utils/keys.ts
@@ -1,5 +1,5 @@
 
-export const openAIapiKey = process.env.OPENAI_API_KEY || "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";
+export const openAIapiKey = process.env.OPENAI_API_KEY;
 export  const pineconeApiKey = process.env.PINECONE_API_KEY || "23e48064-f588-4271-9ce2-632ade12ebcd";
 export const pineconeEnvironment = process.env.PINECONE_ENVIRONMENT || "asia-southeast1-gcp-free"
 export  const pineconeIndexName = process.env.PINECONE_INDEX_NAME || "langchainchatbot"
